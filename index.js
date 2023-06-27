726e5bc0f1bcc86b6b40bae78fa92d3e0597f4e2
Vivek Patel <vpatel360@gmail.com>
2023-06-27 10:48:45 -0400

    commit tree 74a52b8264515066d493c1a67e5b825523796b24

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(array){
+    return (array.find((item)=> item.result === "W")).year
+
+    //return !!win ? win.year : undefined
+    // if (win === undefined){
+    //     return win
+    // }
+    // else {
+    //     return win.year
+    // }
+}
